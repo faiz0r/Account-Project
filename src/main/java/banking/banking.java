b9ccbf5fe3c7ea1ce0bd15292cd9239d8c428634
faiz0r <faizanz94@hotmail.com>
2018-11-13 10:56:35 +0000

    third commit

--- src/main/java/banking/banking.java
+++ src/main/java/banking/banking.java
@@ -3,8 +3,16 @@ package banking;
 public class banking {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Welcome to Banking");
+		System.out.println("Account Project");
+		
+		person Faizan = new person("Faizan", "Zaheer");
+		service s = new service();
+		s.addUser(Faizan);
+		
 
+		//System.out.println("Hello" + " '" + p.getFirstname() + " " + p.getLastName() + "' and Welcome to Banking");
+		
+		
 	}
 
 }
